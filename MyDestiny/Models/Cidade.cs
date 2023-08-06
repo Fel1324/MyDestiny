a47d0b0dee374ede7c4a39ff0aa08b8c27201dd5
Rafael R. de Oliveira <rafael.roberto200618@gmail.com>
2023-08-06 14:34:34 -0300

    todos os dados das cidades foram cadastrados no JSON e foram criadas algumas classes

---
+++ MyDestiny/Models/Cidade.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDestiny.Models
+{
+    public class Cidade
+    {
+        public string Imagem {get; set;}
+        public string Nome {get; set;}
+        public List<string> PontosTuristicos {get; set;}
+    }
+}
---
+++ MyDestiny/Models/CidadeDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDestiny.Models
+{
+    public class CidadeDto
+    {
+        public List<Cidade> Cidades {get; set;}
+    }
+}
