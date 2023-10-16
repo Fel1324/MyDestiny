8d998e5a4bb1e3b317e4c7910d19ac27bb450394
Rafael R. de Oliveira <rafael.roberto200618@gmail.com>
2023-10-16 17:49:43 -0300

    finalizado o carrosel da home

--- MyDestiny/wwwroot/js/carousels.js
+++ MyDestiny/wwwroot/js/carousels.js
@@ -1,12 +1,38 @@
-const slider = document.querySelector('.slider__container');
-let sliderImages = document.querySelector('.slider__images');
+const $slider = document.querySelector('.slider__container');
+let sliderImages = document.querySelectorAll('.slider__images');
 let sliderImagesLast = sliderImages[sliderImages - 1];
 
-const btnNext = document.querySelector('#btn-next');
-const btnPrev = document.querySelector('#btn-prev');
+const $btnNext = document.querySelector('#btn-next');
+const $btnPrev = document.querySelector('#btn-prev');
 
-btnNext.addEventListener('click', () => {
-    let sliderImagesFirst = document.querySelector('.slider__images')[0];
+function next(){
+    let $sliderImagesFirst = document.querySelectorAll('.slider__images')[0];
 
-    slider.style.transform.translate = '-100%';
+    $slider.style.marginLeft = '-200%';
+    $slider.style.transition = 'margin-left 1s';
+    setTimeout(() => {
+        $slider.style.transition = 'none';
+        $slider.insertAdjacentElement('beforeend', $sliderImagesFirst);
+        $slider.style.marginLeft = '-100%';
+    }, 1000);
+}
+
+function prev(){
+    let sliderImages = document.querySelectorAll('.slider__images');
+    let sliderImagesLast = sliderImages[sliderImages.length - 1];
+    $slider.style.marginLeft = '0';
+    $slider.style.transition = 'margin-left 1s';
+    setTimeout(() => {
+        $slider.style.transition = 'none';
+        $slider.insertAdjacentElement('afterbegin', sliderImagesLast);
+        $slider.style.marginLeft = '-100%';
+    }, 1000);
+}
+
+$btnNext.addEventListener('click', function (){
+    next();
+});
+
+$btnPrev.addEventListener('click', function (){
+    prev();
 });
