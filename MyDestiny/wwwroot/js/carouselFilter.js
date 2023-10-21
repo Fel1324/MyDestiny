7dfdddbfdd1a26f0598a7ccd73a74af81d2839c3
Rafael R. de Oliveira <rafael.roberto200618@gmail.com>
2023-10-21 14:23:01 -0300

    finalizado o carrossel dos filtros das atrações

--- MyDestiny/wwwroot/js/carouselFilter.js
+++ MyDestiny/wwwroot/js/carouselFilter.js
@@ -1,37 +1,12 @@
-const slider = document.querySelector('.cities__buttons');
-const prevButton = document.getElementById('carouselF-prev');
-const nextButton = document.getElementById('carouselF-next');
-console.log(slider);
+function direcao(e){
+    let direcao = document.getElementById('carouselFilter');
+    
+    if(e == 1){
+        // esquerda
+        direcao.scrollLeft = direcao.scrollLeft - 200;
 
-nextButton.addEventListener('click', () => {
-    const firstTopic = slider.firstElementChild;
-    slider.appendChild(firstTopic);
-    slider.style.transition = "transform 0.3s ease";
-    slider.style.transform = "translateX(0)";
-    updateButtonVisibility();
-});
-
-prevButton.addEventListener('click', () => {
-    const lastTopic = slider.lastElementChild;
-    slider.prepend(lastTopic);
-    slider.style.transition = "transform 0.3s ease";
-    slider.style.transform = "translateX(0)";
-    updateButtonVisibility();
-});
-
-function updateButtonVisibility() {
-    if (slider.firstElementChild === slider.firstElementChild.nextElementSibling) {
-        prevButton.style.display = "none";
-    } else {
-        prevButton.style.display = "block";
-    }
-
-    if (slider.lastElementChild === slider.lastElementChild.previousElementSibling) {
-        nextButton.style.display = "none";
-    } else {
-        nextButton.style.display = "block";
+    } else if(e == 2){
+        // direita
+        direcao.scrollLeft = direcao.scrollLeft + 200;
     }
 }
-
-// Chame a função de atualização inicialmente
-updateButtonVisibility();
