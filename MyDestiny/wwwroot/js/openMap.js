b4cb643e91791dfe46c233213cbaa10e5ce411ed
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2023-11-26 13:00:51 -0300

    código, da funcionalidade de abrir mapa, refatorado

--- MyDestiny/wwwroot/js/openMap.js
+++ MyDestiny/wwwroot/js/openMap.js
@@ -1,15 +1,20 @@
 const btnOpenMap = document.querySelector('#btnOpenMap');
 const btnCloseMap = document.querySelector('#btnCloseMap');
 const map = document.querySelector('.city-map');
+const mapOverlay = document.querySelector('#map-overlay');
 
 btnOpenMap.addEventListener('click', () => {
     map.classList.add('city-map--open');
     btnCloseMap.style.display = 'block';
+    mapOverlay.style.display = 'block';
+
     document.body.style.overflow = 'hidden';
 });
 
-map.addEventListener('click', () => {
+btnCloseMap.addEventListener('click', () => {
     map.classList.remove('city-map--open');
     btnCloseMap.style.display = 'none';
+    mapOverlay.style.display = 'none';
+
     document.body.style.overflow = 'auto';
 });
