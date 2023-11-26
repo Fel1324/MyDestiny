dc4bccc37b64db86a197de5b408b4c00c8cc8294
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2023-11-26 11:56:08 -0300

    por enquanto, a funcionalidade de abrir mapa está pronta

--- MyDestiny/wwwroot/js/openMap.js
+++ MyDestiny/wwwroot/js/openMap.js
@@ -1,1 +1,15 @@
 const btnOpenMap = document.querySelector('#btnOpenMap');
+const btnCloseMap = document.querySelector('#btnCloseMap');
+const map = document.querySelector('.city-map');
+
+btnOpenMap.addEventListener('click', () => {
+    map.classList.add('city-map--open');
+    btnCloseMap.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+});
+
+map.addEventListener('click', () => {
+    map.classList.remove('city-map--open');
+    btnCloseMap.style.display = 'none';
+    document.body.style.overflow = 'auto';
+});
